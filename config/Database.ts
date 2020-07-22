d3451f50dc7e09e15bdbaa6b6d37df40b3de5d4a
Mohamad Chaman-Motlagh <m.chamanmotlagh@gmail.com>
2020-07-22 21:46:57 +0430

    Create Database class to connect to mongoDB

---
+++ config/Database.ts
@@ -0,0 +1,42 @@
+import * as process from "process";
+import mongoose from "mongoose";
+
+const DBUser: string = "mohamad" || process.env.dbuser;
+const DBPass: string = "1234" || process.env.dbpass;
+const DBAddress: string = "localhost/Test" || process.env.dbaddress;
+
+/**
+ * This is singleton database class
+ * On first instantiation, it connects to mongoDB using mongoose
+ */
+class Database {
+    private readonly _DBUser: string = DBUser;
+    private readonly _DBPassword: string = DBPass;
+    private readonly _DBAddress: string = DBAddress;
+    private readonly _mongoURI: string = "mongodb://" + this._DBUser + ":" + this._DBPassword +
+        "@" + this._DBAddress;
+
+    private static _instance: Database;
+    public static getInstance(){
+        if(Database._instance) {
+            return this._instance;
+        }
+        return new Database();
+    }
+
+    private constructor() {
+        // Connecting to Database
+        mongoose
+            .connect(this._mongoURI)
+            .then(()=> console.log("Mongo connected"))
+            .catch(err => console.log("Error in mongo connection", err));
+        //
+    }
+    public get DBAddress(): string{
+        return this._DBAddress;
+    }
+    public get DBUser(): string{
+        return  this._DBUser;
+    }
+}
+export {Database};
--- src/App.ts
+++ src/App.ts
@@ -1,8 +1,11 @@
 import express, { Application, Request, Response, NextFunction } from "express";
 import bodyParser from "body-parser";
+import {Database} from "../config/Database";
 
 const app: Application = express();
 const port: number = 5000 || process.env.PORT;
+//Connect to database
+Database.getInstance();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,6 +14,7 @@ app.get("/", (req: Request, res: Response, next: NextFunction) => {
     res.send("Hadish sales app");
 });
 
+// Starting sever
 app.listen(port, () => {
     console.log("\x1b[32m", "\x1b[4m", `Server running on ${port}`, "\x1b[0m");
 });
