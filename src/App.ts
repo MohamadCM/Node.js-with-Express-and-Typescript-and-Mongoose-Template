bce06ab13bea0ee33a8f8b548b64d848ee116958
Mohamad ChamanMotlagh <35303473+MohamadCM@users.noreply.github.com>
2021-06-05 13:22:20 +0430

    Update App.ts

--- src/App.ts
+++ src/App.ts
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hadish sales app");
+    res.send("Hello, world!");
 });
 
 // Starting sever
